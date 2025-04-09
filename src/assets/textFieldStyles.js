6e11c82f7ce09d40235de9854857ac57fd4f153a
Arshad Ali Ansari <99089245+arshadaliansari786@users.noreply.github.com>
2025-04-09 15:13:22 +0530

    up

--- src/assets/textFieldStyles.js
+++ src/assets/textFieldStyles.js
@@ -17,27 +17,10 @@ const textFieldStyles = {
         minWidth: "330px",
     },
 };
-// const dropdownStyles = {
-//     width: "360px",
-//     maxWidth: "360px",
-//     minWidth: "360px",
-//     boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-//     borderRadius: "5px",
-//     display: "flex",
-//     "& .MuiInputBase-root": {
-//         width: "100%",
-//         display: "flex",
-//     },
-//     "& .MuiInputBase-input": {
-//         width: "360px",
-//         maxWidth: "360px",
-//         minWidth: "360px",
-//     },
-// };
- const dropdownStyles = {
-    width: "100%", // allow it to grow/shrink within parent
-    maxWidth: "360px", // cap width on large screens
-    minWidth: "0", // allow shrinking on smaller screens
+const dropdownStyles = {
+    width: "100%",
+    // maxWidth: "360px",
+    // minWidth: "360px",
     boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
     borderRadius: "5px",
     display: "flex",
@@ -46,9 +29,30 @@ const textFieldStyles = {
         display: "flex",
     },
     "& .MuiInputBase-input": {
-        width: "100%",
+        width: "360px",
+        maxWidth: "360px",
+        minWidth: "360px",
     },
 };
+//  const dropdownStyles = {
+//     width: "100%", // allow it to grow/shrink within parent
+//     minWidth: "0", // allow shrinking on smaller screens
+//     boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+//     borderRadius: "5px",
+//     display: "flex",
+//     "& .MuiInputBase-root": {
+//         // width: "100%",
+//         height: '44px',
+//         display: "flex",
+//     },
+//     "& .MuiInputBase-input": {
+//         width: "100%",
+//     },
+//     '& .MuiSelect-select': {
+//         padding: '10px 14px',
+//     },
+// };
+
 
 const labelSize = {
     fontSize: "1rem"
